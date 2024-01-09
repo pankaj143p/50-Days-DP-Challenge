1a4ced329332e14e86dd8778d27beb126ab2d521
Pankaj Prajapati <114794372+pankaj143p@users.noreply.github.com>
2024-01-10 00:06:41 +0530

    Day 9 add

---
+++ Day-9/day9.java
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int n = sc.nextInt();
+                    Solution ob = new Solution();
+                    System.out.println(ob.maxSum(n));
+                }
+        }
+}    
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int maxSum(int n) 
+    { 
+        //code here.
+        int dp[]=new int[n+1];
+        dp[0]=0;
+        for(int i=1;i<=n;i++){
+            dp[i]=Math.max(i,dp[i/2]+dp[i/3]+dp[i/4]);
+        }
+        return dp[n];
+    } 
+}
